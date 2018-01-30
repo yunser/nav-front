272a8238576c85794e156a3a729f8442b2505c67
gitcjh <1418503647@.qq.com>
2018-01-30 18:01:54 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <my-page title="云设导航">
+        这是首页内容
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
